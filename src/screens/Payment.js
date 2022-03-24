b56662a365e1619b08cbaf002e06fca37ad8c51e
Tinh Le <ltrongtinh.dev@gmail.com>
2022-03-24 21:13:16 +0700

    [Fix] uri

--- src/screens/Payment.js
+++ src/screens/Payment.js
@@ -1,6 +1,6 @@
 const Payment = () => {
     return(
-        <iframe title="Hello AI" src="https://623c736c179eee7131db2595--unique-lamington-245896.netlify.app/" allow="camera" width="600" height="600">
+        <iframe title="Hello AI" src="https://unique-lamington-245896.netlify.app/" allow="camera" width="600" height="600">
 
         </iframe>
     )
